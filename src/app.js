844c17d0ed9721b0565defa09c4bd170bc47ccfc
jnavarro30 <jnavarro30@live.com>
2023-09-24 23:37:34 -0700

    1.6 error handling

--- src/app.js
+++ src/app.js
@@ -25,4 +25,24 @@ app.get("/say/:greeting", (req, res) => {
   res.send(content);
 });
 
+app.get("/states/:abbreviation", (req, res, next) => {
+  const abbreviation = req.params.abbreviation;
+  if (abbreviation.length !== 2) {
+    next("State abbreviation is invalid.");
+  } else {
+    res.send(`${abbreviation} is a nice state, I'd like to visit.`);
+  }
+});
+
+// Not-found handler
+app.use((req, res, next) => {
+  res.send(`The route ${req.path} does not exist!`);
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.send(err);
+});
+
 module.exports = app;
