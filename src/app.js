cad8f309fd4f1d8b86f9b3cc0934dedceda0e463
jnavarro30 <jnavarro30@live.com>
2023-09-24 20:34:01 -0700

    1.5 query and route params

--- src/app.js
+++ src/app.js
@@ -2,10 +2,6 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
-const sayHello = (req, res, next) => {
-  res.send("Hello!");
-};
-
 const logging = (req, res, next) => {
   console.log("A request is being made!");
   next();
@@ -14,6 +10,19 @@ const logging = (req, res, next) => {
 // app.use(logging);
 app.use(morgan("dev"));
 
-app.get("/hello", sayHello);
+app.get("/hello", (req, res) => {
+  console.log(req.query);
+  const name = req.query.name;
+  const content = name ? `Hello, ${name}!` : "Hello!";
+  res.send(content);
+});
+
+app.get("/say/:greeting", (req, res) => {
+  const greeting = req.params.greeting;
+  const name = req.query.name;
+
+  const content = greeting && name ? `${greeting}, ${name}!` : `${greeting}!`;
+  res.send(content);
+});
 
 module.exports = app;
